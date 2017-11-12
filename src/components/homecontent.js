5a797b02a8ce54ea35eff3d0108007e81c923bda
dgsilcox <dgsilcox@yahoo.com>
2017-11-12 11:28:55 -0500

    crop images and clean up css

--- src/components/homecontent.js
+++ src/components/homecontent.js
@@ -3,10 +3,8 @@ import React, { Component } from 'react';
 export default class Body extends Component {
   render() {
     return (
-
         <div className="homePage" >
-            <img src="img/brisbane_leaves.gif"/>
-
+            <img src="img/brisbane_leaves_1.gif"/>
             <span className="homeText">
                <span className="homeTextTop"><p>Private, custom dog training based on trust and cooperation</p></span>
                <span className="homeTextBottom"><p>Let’s start the journey to your best dog, today!</p></span>
