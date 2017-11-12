ea43ee923ea2b3594e97ee2842e569d12c7e5fc1
Michelle Mullins <dgsilcox@yahoo.com>
2017-11-12 12:11:47 -0500

    add services content

--- src/components/servicescontent.js
+++ src/components/servicescontent.js
@@ -25,6 +25,44 @@ export default class Body extends Component {
           </tr>
         </table>
       </div>
+
+      <p>
+
+      Private In-Home Training
+      New Puppy Consult –  Initial 1 ½ hour consult $125.00
+      Puppies younger than 5 months. 1 ½ hour consult on the new puppy you plan to add or have added to your family. We will discuss the basic training you want your new puppy to have, what activities your new puppy might enjoy based on your pup’s personality and breed/mix, introductions to current family pets, and provide a quick start training demo for your entire family.
+
+      New Puppy Package – Five Custom Sessions $575.00
+      	Puppies younger than 5 months. Five sessions - Includes the New Puppy Intro consult plus four additional customized, one hour training sessions for you and any family members. Together we will select the skills you want your puppy to learn and design a training plan to get you on your way to a great relationship with your puppy.
+
+      New Dog Consult – Initial 1 ½ hour consult $125.00
+      	Dogs over 5 months and older. 1 ½ hour consult on the new dog you plan to add or have added to your family. We will discuss the basic training you want your new dog to have, introductions to current family pets, what behaviors your new dog might enjoy based on your dog’s personality and breed/mix, and provide a quick start training demo for your entire family.
+
+      New Dog Package – Five Custom Sessions $575.00
+      	Dogs over 5 months and older. Five sessions - Includes the New Dog Intro consult plus four additional customized training sessions for you and any family members. Together we will choose the basic skills you want your new dog to learn and design a training plan to get you on your way to a great relationship with your new dog.
+
+      Handle With Care Intro – Initial 1 hour consult $125.00
+      	Want your dog to enjoy and participate in essential care activities? Does your pet suffer stress, anxiety or fear associated with veterinary and grooming procedures as well as the transportation to and from the facilities? This one hour consult includes discussion of  training goals and resources on bathing, transportation, nail trimming, examinations, injections, brushing and many other grooming and veterinary procedures. The goal is to teach pets to happily cooperate in their own care and relieve the fear, anxiety and stress they have to create a safe, comfortable experience
+      .
+      Handle With Care Puppy/Kitten Package – Four Custom Sessions $500.00
+      	Pets under 5 months. Start your new family member off with skills that are essential and will prepare you both for a lifetime of quality care. Includes the Handle With Care Intro consult plus three additional customized training sessions designed to teach puppies and kittens to happily cooperate in their own care and relieve the fear, anxiety and stress they have to create a safe, comfortable experience. This packaged will be tailored to include transportation, nail trimming, examinations, injections, brushing, bathing and many other grooming and veterinary procedures your pet finds concerning or stressful.
+
+      Handle With Care Dog/Cat Package – Four Custom Sessions $500.00
+      	Pets over 5 months. Start your new family off with skills that are essential and will prepare you both for a lifetime of quality care Includes the Handle With Care Intro consult plus three additional customized training sessions designed to teach pets to happily cooperate in their own care and relieve the fear, anxiety and stress they have to create a safe, comfortable experience. This packaged will be tailored to include transportation, nail trimming, examinations, injections, brushing and any other grooming and veterinary procedures your pet finds concerning or stressful.
+
+      Single Private Training Sessions – 1 hour per session $125.00
+
+
+      Offerings in Partnership with Veterinary Clinics
+      Empowered Puppy Handling & Socialization Classes – Small class sizes for plenty of individual attention - $120.00
+      	Begin with our intro video followed by four, weekly classes to get you and your puppy on the road to a happy, healthy life together. Class will include basic skills for grooming and examinations which will be a skills valued for a lifetime of care and socialization, which is of upmost importance for puppies. Puppies age 8 to 14 weeks.
+
+      Pet Professional Workshops – Cooperative Care and Handling
+      Looking to bond your customers and patients to you and your business or practice? Want to promote low stress patient visits or grooming? The most innovative businesses are integrating procedures and handling skills that bring back the enjoyment of working with animals? Honest To Dog’s Fear Free® Certified trainer can help train your team in the hands on skills to make this a reality. Workshops are available for small or large groups in veterinary, grooming and daycare settings. Contact us for a list of available workshops and private sessions.
+
+
+
+      </p>
       </div>
     );
   }
