cff78d9b8761e19117d9fa3683394343c1873695
dgsilcox <dgsilcox@yahoo.com>
2017-11-12 14:46:04 -0500

    add facebook link to header

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
       <div className="header">
         <div className="topLine">&nbsp;</div>
         <div className="logo"><img src="img/HonestToDogLogoRGBko.png" alt="Honest To Dog"/></div>
-        <div className="contactInfo"> Michelle Mullins<br/><a href="tel:804-405-4494">804.405.4494</a><br/> <a href="mailto:michelle@honesttodogrva.com">michelle@honesttodogrva.com</a></div>
+        <div className="contactInfo"> Michelle Mullins<br/><a href="tel:804-405-4494">804.405.4494</a><br/> <a href="mailto:michelle@honesttodogrva.com">michelle@honesttodogrva.com</a><br/><a href="https://www.facebook.com/honesttodogrva/">facebook.com/honesttodogrva</a></div>
         <div className="clear"/>
       </div>
     );
