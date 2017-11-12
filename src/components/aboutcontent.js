11f2e6cc37c0ec743422ccab8e9ee41312546d3c
dgsilcox <dgsilcox@yahoo.com>
2017-11-11 19:21:43 -0500

    Clean up margins and responsive design

--- src/components/aboutcontent.js
+++ src/components/aboutcontent.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
+
       <div>
-      <span className="aboutLeft"><img src="img/Michelle and Jinks3.1.jpg"/></span>
+      <span className="aboutLeft"><img src="img/Michelle-and-Jinks3.1.gif"/></span>
       <span className="aboutRight">
         <p>Hello, I’m Michelle Mullins, KPA CTP, CPDT-KSA</p>
-        <p>I have over 15 years of experience in the pet training industry and I would love to share that experience with you and your family pet. All my training has the same goal of developing long, enriching relationships between pets and their people. Michelle’s mission is to teach you to communicate with and train your pet while developing the skills you can apply to all future pets.</p>
+        <p>I have over 15 years of experience in the pet training industry and I would love to share that experience with you and your family pet.</p>
+      </span>
+
+      <span className="aboutBottom">
+        <p>All my training has the same goal of developing long, enriching relationships between pets and their people. Michelle’s mission is to teach you to communicate with and train your pet while developing the skills you can apply to all future pets.</p>
         <p>In addition to being a KPA – CTP, she is a CPDT –KA, Certified Professional Dog Trainer – Knowledge Assessed and TAGTeach Primary Level Certified.</p>
         <p>Michelle’s education includes the Karen Pryor Academy, Purdue University’s DOGS Course, University of Richmond’s Canine Logic and Advanced Canine Logic courses and hundreds of hours of seminars and workshops. Michelle believes continuing to seek out advanced education in her field provides her clients, both human and canine, the best training available.</p>
         <p>Michelle shares her life with her husband of 25 years and two wonderful dogs.</p>
-</span>
-      </div>
+      </span>
+    </div>
     );
   }
 }
