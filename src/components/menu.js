53bf529810b9b4b4f491bb4843de14dfa7105e0f
dgsilcox <dgsilcox@yahoo.com>
2017-11-19 09:26:13 -0500

    drop down one

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown'
+
+const options = [
+  'one', 'two', 'three'
+]
+const defaultOption = options[0]
 
 export default class Menu extends Component {
+
   render() {
     return (
       <div className="menu">
+
+<Dropdown options={options} onChange={this._onSelect}
+value={defaultOption}
+placeholder="Select an option" />
+
+{/*
             <span className="menuItem">&nbsp;</span>
             <span className="menuItem"><a href="/home">home</a></span>
             <span className="menuItem"><a href="/goals">mission</a></span>
             <span className="menuItem"><a href="/about">about</a></span>
             <span className="menuItem"><a href="/services">services</a></span>
             <span className="menuItem"><a href="/veterinary">professionals</a></span>
+
+*/}
         <div className="clear"/>
+
       </div>
 
     );
